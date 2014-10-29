9a2ef3c101aa812eaf862a6d1d1230d403ad7fff
mrnutz <mrnutz@users.noreply.github.com>
2014-10-29 16:06:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ SRC="./src"
 cp ${SRC}/bashrc ${HOME}/.bashrc
 cp ${SRC}/tmux ${HOME}/.tmux.conf
 cp ${SRC}/vimrc ${HOME}/.vimrc
+
+mkdir -p ${HOME}/.config/fontconfig
+cp fonts.conf ${HOME}/.config/fontconfig/
