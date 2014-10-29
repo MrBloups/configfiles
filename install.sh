d6bed567f1670c69f0ff08e2a0f04e97cf008e8d
mrnutz <mrnutz@users.noreply.github.com>
2014-10-29 16:12:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ cp ${SRC}/tmux ${HOME}/.tmux.conf
 cp ${SRC}/vimrc ${HOME}/.vimrc
 
 mkdir -p ${HOME}/.config/fontconfig
-cp fonts.conf ${HOME}/.config/fontconfig/
-cp gtkrc-2.0 ${HOME}/.gtkrc-2.0
+cp ${SRC}/fonts.conf ${HOME}/.config/fontconfig/
+
+cp ${SRC}/gtkrc-2.0 ${HOME}/.gtkrc-2.0
+
+mkdir -p ${HOME}/.config/gtk-3.0
+cp ${SRC}/settings.ini ${HOME}/.config/gtk-3.0/
