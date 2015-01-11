20f641ef2c7420cd39bf0063d8644207b8a1613f
MrBloups <jrm.dsr@gmail.com>
2015-01-11 17:21:49 +0100

    Reorganization of i3 directory

---
+++ i3/laptop/i3battery.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <i3 MODE>"
+    exit 1
+fi
+
+MODE="$1"
+
+STATUS="Discharging"
+TIMER=5
+MINI=5
+
+PATH=/sys/class/power_supply/BAT1
+
+show=false
+
+while true
+do
+    bcapacity=`/usr/bin/cat $PATH/capacity`
+    bstatus=`/usr/bin/cat $PATH/status`
+
+    if [ "$bstatus" = "$STATUS" ]; then
+        if [ "$bcapacity" -le $MINI ]; then
+            if [ "$show" != true ]; then
+                /usr/bin/i3-msg mode $MODE
+                show=true
+            fi
+        fi
+    fi
+
+    /usr/bin/sleep $TIMER
+done
+
+exit 0
