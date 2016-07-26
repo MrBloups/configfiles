60a99849e8ffc0047c468cd01f0ef92b888807fc
mrbloups <jrm.dsr@gmail.com>
2016-07-26 21:32:23 +0200

    add powersave tweaks

---
+++ ideapad-S300/grub.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+grub_file='/etc/default/grub'
+
+sed 's/GRUB_TERMINAL_OUTPUT/ s/^/#/' $grub_file
+sed 's/GRUB_CMDLINE_LINUX/ s/quiet/quiet pcie_aspm=force i915.modeset=1 i915.semaphores=1 i915.enable_rc6=1 i915.enable_fbc=1/' $grub_file
+
+exit 0
