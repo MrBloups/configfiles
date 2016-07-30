b9326b8eb3ee070427894d1486df22b09860a546
mrbloups <jrm.dsr@gmail.com>
2016-07-30 08:49:27 +0200

    some updates

--- ideapad-S300/grub.sh
+++ ideapad-S300/grub.sh
@@ -3,6 +3,6 @@
 grub_file='/etc/default/grub'
 
 sed 's/GRUB_TERMINAL_OUTPUT/ s/^/#/' $grub_file
-sed 's/GRUB_CMDLINE_LINUX/ s/quiet/quiet pcie_aspm=force i915.modeset=1 i915.semaphores=1 i915.enable_rc6=1 i915.enable_fbc=1/' $grub_file
+sed 's/GRUB_CMDLINE_LINUX/ s/quiet/quiet i915.modeset=1 i915.semaphores=1 i915.enable_rc6=1 i915.enable_fbc=1/' $grub_file
 
 exit 0
